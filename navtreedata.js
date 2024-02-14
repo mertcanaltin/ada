b402bb3a1c7e3eabc2ce1023b1c27797a11a965d
mertcanaltin <mertcanaltin@users.noreply.github.com>
2024-02-14 09:00:41 +0000

    deploy: 0c001a586a13cdeb29e999066f9667a4bc483dc1

---
+++ navtreedata.js
@@ -0,0 +1,91 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "Ada", "index.html", [
+    [ "Ada is fast.", "index.html#autotoc_md13", null ],
+    [ "Quick Start", "index.html#autotoc_md14", null ],
+    [ "Bindings of Ada", "index.html#autotoc_md15", null ],
+    [ "Usage", "index.html#autotoc_md16", [
+      [ "Requirements", "index.html#autotoc_md12", null ],
+      [ "Parsing & Validation", "index.html#autotoc_md17", null ],
+      [ "Examples", "index.html#autotoc_md18", null ],
+      [ "C wrapper", "index.html#autotoc_md19", null ],
+      [ "CMake dependency", "index.html#autotoc_md20", null ]
+    ] ],
+    [ "Installation", "index.html#autotoc_md21", [
+      [ "Homebrew", "index.html#autotoc_md22", null ]
+    ] ],
+    [ "Contributing", "index.html#autotoc_md23", [
+      [ "Building", "index.html#autotoc_md24", null ],
+      [ "Amalgamation", "index.html#autotoc_md25", null ],
+      [ "License", "index.html#autotoc_md26", null ]
+    ] ],
+    [ "Command line interface (CLI)", "db/d8a/md_docs_2cli.html", null ],
+    [ "Namespaces", "namespaces.html", [
+      [ "Namespace List", "namespaces.html", "namespaces_dup" ],
+      [ "Namespace Members", "namespacemembers.html", [
+        [ "All", "namespacemembers.html", null ],
+        [ "Functions", "namespacemembers_func.html", null ],
+        [ "Variables", "namespacemembers_vars.html", null ],
+        [ "Typedefs", "namespacemembers_type.html", null ],
+        [ "Enumerations", "namespacemembers_enum.html", null ],
+        [ "Enumerator", "namespacemembers_eval.html", null ]
+      ] ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", null ],
+        [ "Functions", "functions_func.html", null ],
+        [ "Variables", "functions_vars.html", null ],
+        [ "Related Symbols", "functions_rela.html", null ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ],
+      [ "File Members", "globals.html", [
+        [ "All", "globals.html", "globals_dup" ],
+        [ "Functions", "globals_func.html", null ],
+        [ "Typedefs", "globals_type.html", null ],
+        [ "Macros", "globals_defs.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"annotated.html",
+"d5/ddb/namespaceada_1_1idna.html#ac7495198fe0aa95543b347ac9cc548dd",
+"da/d69/character__sets-inl_8h.html#aec120154efccb7b70bd36785bf6a71b7",
+"dd/d03/structada_1_1url.html#afb1751e62c4d2cb124108a3fb4c1618a",
+"dd/dd6/ada__c_8cpp.html#a4ed6111d02ff556f686799c482076e5e"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
